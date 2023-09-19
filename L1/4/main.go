e67eb9b066806d83f28bbbe9b98b43820492ef58
Grandpa Trula <grandpat@un-d6.kzn.21-school.ru>
2023-09-19 23:04:50 +0300

    https://blog.ildarkarymov.ru/posts/graceful-shutdown/

--- L1/4/main.go
+++ L1/4/main.go
@@ -4,9 +4,13 @@ import (
 	"context"
 	"fmt"
 	"os"
+	"os/signal"
 	"sync"
+	"syscall"
 	"time"
 )
+
+// todo попросить проверить, котого поопытнее
 
 func main() {
 	fmt.Println("Напиши количество воркеров")
@@ -20,11 +24,12 @@ func main() {
 // По наступлению временной отметки или дедлайна (context.WithDeadline)
 func exOne(N int) {
 	var wg sync.WaitGroup
-	ctx := context.Background() // родительский контест
+	ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM) // контекст для изящного завершения
+	defer stop()
 	input := make(chan int)
 	for i := 1; i <= N; i++ {
 		wg.Add(1)
-		go worker(ctx, &wg, input)
+		go worker(ctx, &wg, input) //
 	}
 	go write(ctx, input)
 	wg.Wait()
@@ -34,11 +39,11 @@ func worker(ctx context.Context, wg *sync.WaitGroup, input <-chan int) {
 	defer wg.Done()
 	for {
 		select {
-		case <-ctx.Done():
+		case <-ctx.Done(): // завершение работы
 			return
 		default:
-			if val, ok := <-input; ok {
-				time.Sleep(1 * time.Second)
+			if val, ok := <-input; ok { // считываем пока в канал поступают данные
+				//time.Sleep(1 * time.Second)
 				fmt.Println(val)
 			}
 		}
@@ -46,17 +51,16 @@ func worker(ctx context.Context, wg *sync.WaitGroup, input <-chan int) {
 }
 
 func write(ctx context.Context, input chan<- int) {
-	defer close(input)
+	defer close(input) // выполняеться при выходе из функции
 	i := 1
 	for {
 		select {
-		case <-ctx.Done():
+		case <-ctx.Done(): // завершение работы
 			return
 		default:
 			i *= 2
-			input <- i
+			input <- i // записываеи данные в канал
 			time.Sleep(1 * time.Second)
 		}
-
 	}
 }
