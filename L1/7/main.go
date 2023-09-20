f3795947638476909123d8c4b7a64b58d4ee2d70
Grandpa Trula <grandpat@un-d6.kzn.21-school.ru>
2023-09-20 21:49:13 +0300

    https://dev.to/karanpratapsingh/sync-package-in-go-1bkn

--- L1/7/main.go
+++ L1/7/main.go
@@ -7,6 +7,12 @@ import (
 )
 
 func main() {
+	exOne()
+	exTwo()
+
+}
+
+func exOne() {
 	mp := make(map[int]int)
 	// будем использовать мютекс для конкурентной записи
 	var mutex sync.Mutex
@@ -22,4 +28,23 @@ func main() {
 	}
 	wg.Wait()
 	fmt.Println(mp)
+}
+
+// sync.Map — это примерно как map[any]any, но она готова к конкуретному доступу,
+// т.е. её нет необходимости обкладывать блокировками.
+func exTwo() {
+	var mm sync.Map
+	var wg sync.WaitGroup
+
+	for i := 1; i <= 3; i++ {
+		wg.Add(1)
+		go func(k int) {
+			defer wg.Done()
+			mm.Store(k, k*2) // заполнени
+		}(i)
+	}
+	wg.Wait()
+	for k := 1; k <= 3; k++ {
+		fmt.Println(mm.Load(k)) // выводсодержимого по ключу
+	}
 }
