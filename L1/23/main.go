390e7f0736fa5ba1b36c4cc2981ba3753d9d5dfb
Grandpa Trula <grandpat@un-d6.kzn.21-school.ru>
2023-09-22 17:33:40 +0300

    20_done

---
+++ L1/23/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	alf := []string{"a", "b", "c", "d", "f"}
+	fmt.Println(delete(alf, 2))
+
+}
+
+func delete(alf []string, i int) (tmp []string) {
+	tmp = alf[:i]
+	tmp = append(tmp, alf[i:]...)
+	return
+}
