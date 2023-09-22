a16cd03de845816582e48b48cc7649d8793eb1a2
Grandpa Trula <grandpat@un-d6.kzn.21-school.ru>
2023-09-22 10:54:57 +0300

    https://blog.boot.dev/golang/quick-sort-golang/

--- L1/16/main.go
+++ L1/16/main.go
@@ -1,3 +1,5 @@
+// Реализовать быструю сортировку массива (quicksort)
+// встроенными методами языка.
 package main
 
 import (
@@ -5,8 +7,26 @@ import (
 )
 
 func main() {
-	arr := []int{2, 17, 27, 11, -9, 108}
-	fmt.Println(arr)
+	arr := []int{2, 17, 27, 11, -9, 14}
+	fmt.Println(quickSort(arr))
 }
 
-// todo
+func quickSort(arr []int) []int {
+	if len(arr) < 2 {
+		return arr
+	}
+	var (
+		less []int
+		more []int
+	)
+	pivot := len(arr) - 1
+	more = append(more, arr[pivot])
+	for c := 0; c < pivot; c++ {
+		if arr[c] <= arr[pivot] {
+			less = append(less, arr[c])
+		} else {
+			more = append(more, arr[c])
+		}
+	}
+	return append(quickSort(less), quickSort(more)...)
+}
