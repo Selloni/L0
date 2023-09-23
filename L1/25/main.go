8507102a26914759dd569b0b83649a97f761abef
Grandpa Trula <grandpat@un-d6.kzn.21-school.ru>
2023-09-24 00:31:00 +0300

    done_26

--- L1/25/main.go
+++ L1/25/main.go
@@ -10,22 +10,29 @@ import (
 
 func main() {
 	fmt.Println("start")
-	SleepOne(3)
-	fmt.Println("end")
+	SleepOne(2)
+	fmt.Println("sleep 2 sec")
+	SleepTwo(2)
+	fmt.Println("end 2")
 }
 
 // простая реализация на контекстах
 func SleepOne(sec time.Duration) {
 	ctx, cancel := context.WithTimeout(context.Background(), sec*time.Second) // через sec* отправим в канал сигнал
 	defer cancel()
-	work(ctx)
+	func() {
+		for {
+			select {
+			case <-ctx.Done(): // программа в цикле ждет чтения из канал
+				return
+			}
+		}
+	}()
 }
 
-func work(ctx context.Context) {
-	for {
-		select {
-		case <-ctx.Done(): // программа в цикле ждет чтения из канал
-			return
-		}
+func SleepTwo(sec time.Duration) {
+	tt := time.Now()
+	ll := tt.Add(2 * time.Second)
+	for time.Now().Before(ll) {
 	}
 }
--- L1/26/main.go
+++ L1/26/main.go
@@ -1,5 +1,40 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"strings"
+)
 
+func main() {
+	fmt.Println(checkOne("abcAdkqL"))
+	fmt.Println(checkOne("abc213"))
+	fmt.Println(checkOne("afsd232"))
+	fmt.Println(checkTwo("abcAdkqL"))
+	fmt.Println(checkTwo("abc213"))
+	fmt.Println(checkTwo("afsd232"))
+}
+
+func checkOne(str string) bool {
+	mm := make(map[int32]bool)
+	for _, i := range str {
+		if mm[i] == true { // если символ уже был
+			return false
+		}
+		mm[i] = true    // встречаем символ
+		mm[i-32] = true // топорный способ, не оптимизированно
+	}
+	return true
+}
+
+// логика как в приемер вышел
+func checkTwo(str string) bool {
+	str = strings.ToUpper(str) // все символы переводим в верхний регистр
+	mm := make(map[rune]struct{})
+	for _, char := range str {
+		if _, ok := mm[char]; ok {
+			return false
+		}
+		mm[char] = struct{}{} // используем структуру, потому что она ни чегоне весит
+	}
+	return true
 }
