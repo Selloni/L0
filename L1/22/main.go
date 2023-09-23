9a0fa8d98408e92228c7a57cb712de975fd11295
Grandpa Trula <grandpat@un-d6.kzn.21-school.ru>
2023-09-23 18:21:07 +0300

    done_22

--- L1/22/main.go
+++ L1/22/main.go
@@ -1,3 +1,16 @@
 package main
 
-//todo
+import (
+	"fmt"
+	"math/big"
+)
+
+func main() {
+	a := big.NewInt(1234567890123456789)
+	b := big.NewInt(9876543210987654)
+	fmt.Println(new(big.Int).Add(a, b))
+	fmt.Println(new(big.Int).Div(a, b))
+	fmt.Println(new(big.Int).Mul(a, b))
+	fmt.Println(new(big.Int).Sub(a, b))
+
+}
