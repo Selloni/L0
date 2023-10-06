c6c369ed988ce3bba6966d069ae03451f2c917dc
Grandpa Trula <grandpat@un-d6.kzn.21-school.ru>
2023-10-07 01:46:23 +0300

    недоделано

--- L2/pattern/05_chain_of_resp.go
+++ L2/pattern/05_chain_of_resp.go
@@ -1,7 +1,82 @@
 package main
 
+import (
+	"fmt"
+	"time"
+)
+
 /*
-	Реализовать паттерн «цепочка вызовов».
-Объяснить применимость паттерна, его плюсы и минусы, а также реальные примеры использования данного примера на практике.
-	https://en.wikipedia.org/wiki/Chain-of-responsibility_pattern
+	ЦЕПОЧКА ВЫЗОВОВ "Chain of Command"
+	Цепочка обязанностей — это поведенческий паттерн проектирования,
+	который позволяет передавать запросы последовательно по цепочке обработчиков.
+	Каждый последующий обработчик решает, может ли он обработать
+	запрос сам истоит ли передавать запрос дальше по цепи.
+
+	=========================================================
+
+
+
 */
+
+type HandlerI interface {
+	SetNext(handler HandlerI)
+	HandlerI(data *Data)
+}
+
+// Обработка данных
+type Data struct {
+	GetSource    bool // были ли полученны данне
+	UpdateSource bool // обработаны ли данные
+}
+
+type Device struct {
+	Name string
+	Next HandlerI // интерфейс
+}
+
+// происходит обработка данных
+func (d *Device) HandlerI(data *Data) {
+	if data.GetSource {
+		fmt.Println("Данные уже были обработаны")
+		d.Next.HandlerI(data)
+		return
+	}
+	data.GetSource = true
+	d.Next.HandlerI(data)
+	for i := 0; i < 3; i++ {
+		fmt.Println("Данные обрабатываеються ...")
+		time.Sleep(1 * time.Second)
+	}
+}
+
+// передаем данные дальше
+func (d *Device) SetNext(data HandlerI) {
+	d.Next = data
+}
+
+type SaveData struct {
+	Next HandlerI
+}
+
+func (d *SaveData) HandlerI(data *Data) {
+	if data.GetSource {
+		fmt.Println("Данные уже сохранены")
+		return
+	}
+	data.GetSource = true
+	d.Next.HandlerI(data)
+	for i := 0; i < 3; i++ {
+		fmt.Println("Сохроняем данные ...")
+		time.Sleep(1 * time.Second)
+	}
+}
+
+func (d *SaveData) SetNext(data HandlerI) {
+	d.Next = data
+}
+
+func main() {
+	dd := Device{Name: "mySpoon"}
+	save := SaveData{}
+	save.Next.SetNext(&dd)
+}
