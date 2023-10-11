02a34246f1609e482d4e3f1c95d95ea1abfc014f
Grandpa Trula <grandpat@si-d5.kzn.21-school.ru>
2023-10-11 18:34:16 +0300

    основная часть сделана, прситупил к реализации флагов

---
+++ L2/develop/dev05/test.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+go build
+
+ ./dev06 сын text.txt  > s21_grep.txt
+ grep сын text.txt  > grep.txt
+ diff -s s21_grep.txt grep.txt
+ rm s21_grep.txt grep.txt
+
+ ./dev06 int task.go > s21_grep.txt
+ grep int task.go > grep.txt
+ diff -s s21_grep.txt grep.txt
+ rm s21_grep.txt grep.txt
+
+ ./dev06 -v int task.go > s21_grep.txt
+ grep -v int task.go > grep.txt
+ diff -s s21_grep.txt grep.txt
+# rm s21_grep.txt grep.txt
+
+rm -rf dev06
