995e3fdbf56ad01c6f5efa013362818b7754678b
Yakov <vip.petrusev@mail.ru>
2023-10-17 11:26:20 +0300

    dev07

--- L2/develop/dev07/task.go
+++ L2/develop/dev07/task.go
@@ -1,5 +1,11 @@
 package main
 
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
 /*
 === Or channel ===
 
@@ -35,4 +41,45 @@ fmt.Printf(“fone after %v”, time.Since(start))
 
 func main() {
 
+	sig := func(after time.Duration) <-chan interface{} {
+		c := make(chan interface{})
+		go func() {
+			defer close(c)
+			time.Sleep(after)
+		}()
+		return c
+	}
+
+	start := time.Now()
+	<-or(
+		//sig(2*time.Hour),
+		//sig(5*time.Minute),
+		sig(1*time.Second),
+		//sig(1*time.Hour),
+		sig(3*time.Second),
+	)
+	fmt.Printf("fone after %v", time.Since(start))
+
+}
+
+func or(channels ...<-chan interface{}) <-chan interface{} {
+	if len(channels) == 0 {
+		return nil
+	}
+	done := make(chan interface{})
+	wg := sync.WaitGroup{}
+	wg.Add(len(channels))
+	for _, ch := range channels {
+		go func(ch <-chan interface{}) {
+			defer wg.Done()
+			for c := range ch {
+				done <- c
+			}
+		}(ch)
+	}
+	go func() {
+		wg.Wait()
+		close(done)
+	}()
+	return done
 }
