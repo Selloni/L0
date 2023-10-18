ef9ff20d4aaf3303a027d299bc016a1ebb2d5a4f
Grandpa Trula <grandpat@si-d5.kzn.21-school.ru>
2023-10-18 19:57:29 +0300

    реализовать ps

--- L2/develop/dev08/task.go
+++ L2/develop/dev08/task.go
@@ -4,6 +4,9 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"os/exec"
+	"path/filepath"
+	"strconv"
 	"strings"
 )
 
@@ -32,36 +35,85 @@ func shell() {
 	fmt.Print(": ")
 	reader := bufio.NewScanner(os.Stdin)
 	if reader.Scan() {
-		//multCommand := strings.Split(reader.Text(), "|")
-		//for i := range multCommand {
-		//	commandExecution(multCommand[i])
-		//}
 		commandExecution(reader.Text())
 	}
 }
 
 func commandExecution(str string) {
-	//str = strings.TrimSpace(str)
 	command := strings.Split(str, " ")
 	if command[0] == "pwd" {
 		pwd()
 		fmt.Print("\n")
 	} else if command[0] == "cd" {
-
+		cd(command)
 	} else if command[0] == "echo" {
 		echo(command)
 		fmt.Print("\n")
 	} else if command[0] == "kill" {
-
+		if len(command) < 2 {
+			fmt.Printf("kill: not enough arguments")
+			return
+		}
+		kill(command[1:])
 	} else if command[0] == "ps" {
 
 	} else {
-		fmt.Printf("Команда не найдена: %s\n", command[0])
+		fork(command)
 	}
 }
 
-func cd() {
+func fork(command []string) {
+	cmd := exec.Command(command[0], command[1:]...)
+	out, err := cmd.Output()
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(string(out))
+}
 
+func kill(command []string) {
+	for _, s := range command {
+		i, err := strconv.Atoi(s)
+		if err != nil {
+			fmt.Println(err)
+		}
+		proc, err := os.FindProcess(i)
+		if err != nil {
+			fmt.Println(err)
+		}
+		proc.Kill()
+	}
+}
+
+func cd(command []string) {
+	if len(command) == 1 {
+		home, err := os.UserHomeDir()
+		if err != nil {
+			fmt.Println(err)
+		}
+		err = os.Chdir(home)
+		if err != nil {
+			fmt.Println("Error:", err)
+			return
+		}
+	} else if command[1] == ".." {
+		wd, err := os.Getwd()
+		if err != nil {
+			fmt.Println(err)
+		}
+		parent := filepath.Dir(wd)
+		err = os.Chdir(parent)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+	} else {
+		err := os.Chdir(command[1])
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+	}
 }
 
 func pwd() {
